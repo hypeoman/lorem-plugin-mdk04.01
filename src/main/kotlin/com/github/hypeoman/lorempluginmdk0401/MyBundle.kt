78718fafb4c126afe63d6f14e73211898da971b0
GitHub Action <action@github.com>
2023-10-08 14:02:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/hypeoman/lorempluginmdk0401/MyBundle.kt
+++ src/main/kotlin/com/github/hypeoman/lorempluginmdk0401/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hypeoman.lorempluginmdk0401
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hypeoman/lorempluginmdk0401/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hypeoman/lorempluginmdk0401/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hypeoman.lorempluginmdk0401.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
